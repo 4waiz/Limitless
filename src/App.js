441294c005392aaa2b848ebe8b2d3ea3a949f691
4waiz <awaizahmed2005@gmail.com>
2025-05-18 13:55:33 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
+            <BrowserRouter basename={process.env.PUBLIC_URL}></BrowserRouter>
           </Routes>
         </main>
         <Footer />
