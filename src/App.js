6ff1da5242f4419dfaaf766caad8e9eb59306afa
4waiz <awaizahmed2005@gmail.com>
2025-05-18 14:06:27 +0400

    Configure for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/GlobalStyles';  // Updated import
+import GlobalStyles from './styles/GlobalStyles';
 import { theme } from './styles/theme';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header />
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
-            <BrowserRouter basename={process.env.PUBLIC_URL}></BrowserRouter>
           </Routes>
         </main>
         <Footer />
